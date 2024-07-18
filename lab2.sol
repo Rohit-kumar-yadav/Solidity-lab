c8afefebecebde81928214e58c5299f7b98255d8
Rohit kumar yadav <76595116+Rohit-kumar-yadav@users.noreply.github.com>
2024-07-18 20:00:18 +0530

    Create lab2.sol

---
+++ lab2.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract demo {
+    
+    function getLocal() public pure returns(uint){
+    uint rollNumber = 10;
+    return rollNumber;
+}
+}
